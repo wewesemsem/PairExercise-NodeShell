27d8adf0e30cc7d9165750a561faef6dea6af1ce
Emma Munley <emmamunley@gmail.com>
2019-09-23 12:19:06 -0400

    added pwd.js

--- bash.js
+++ bash.js
@@ -1,10 +1,15 @@
 //output a prompt
 process.stdout.write('prompt > ');
 
-//the stdin 'data' even fires after a user types in a line 
-process.stdin.on('data', (data) => {
-    const cmd = data.toString().trim();
+//the stdin 'data' even fires after a user types in a line
+process.stdin.on('data', data => {
+  const cmd = data.toString().trim();
 
+  if (cmd === 'pwd') {
+    const pwdFunc = require('./pwd.js');
+    pwdFunc();
+  } else {
     process.stdout.write('You typed: ' + cmd);
-    process.stdout.write('\nprompt > ');
-})
+  }
+  process.stdout.write('\nprompt > ');
+});
